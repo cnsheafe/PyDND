5aa2d002966c2a57b0a2c9eaf255aa3afc1e837c
cnsheafe <voldaltz@gmail.com>
2016-07-13 19:14:47 -0500

    removed main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import charInterface
-from charSheet import charSheet
-
-print "Welcome to PyDnD, a Python-based character sheet management system for \
-Dungeons&Dragons 5e.\n"
-
-mycmd = input("Type in a command. Use 'help' for a list of commands.\n");
