b4ecd69f1785b6a6698d6fbd67cf061e0189380b
cnsheafe <voldaltz@gmail.com>
2016-07-15 23:20:00 -0500

    added more stats

--- charInterface.py
+++ charInterface.py
@@ -7,7 +7,15 @@ import pprint
 class Interface:
     def __init__(self,**kwargs):
         self.sheet = Sheet()
-    def importStats(self):
+    def initCharacter(self):
+        self.sheet.name = raw_input("What is your character's name? ")
+        self.sheet.race = raw_input("What is your character's race? ")
+        self.sheet.char_class = raw_input("What class are you? ")
+        self.sheet.alignment = raw_input("What is your alignment? ")
+        self.sheet.background = raw_input("What is your character's background? ")
+        self.sheet.lvl = raw_input("What is your level? ")
+        self.sheet.exp = raw_input("How much experience do you have? ")
+
         stat_list = self.sheet.total_stats.keys()
         print "Type in the stat followed by any bonuses for the listed ability. Ex: 14 2"
         for stat in stat_list:
@@ -17,6 +25,19 @@ class Interface:
             self.sheet.base_stats[stat],self.sheet.bonus_stats[stat]=\
             base,bonus
             self.sheet.total_stats[stat] = base+bonus
+
+        self.sheet.p_bonus = 0
+        self.sheet.s_throws = 0
+        self.sheet.skills = 0
+
+        self.sheet.ac = raw_input("What is your AC? ")
+        self.sheet.initiative = raw_input("What is your initiative? ")
+        self.sheet.speed = raw_input("What is your speed? ")
+        self.sheet.maxhp = raw_input("What is your max health? ")
+        self.sheet.hp = 0
+        self.sheet.tmp_hp = 0
+        self.sheet.hit_die = raw_input("What is your hit die? ")
+
     def retrieveSheet(self,filename):
         if not os.path.isfile(filename):
             print "Character sheet does not exist."
@@ -32,7 +53,7 @@ class Interface:
                 o_write = raw_input("Character sheet already exists. Overwrite?[Y/n]")
                 print o_write
                 if o_write == 'Y':
-                    self.importStats()
+                    self.initCharacter()
                     fid = open(filename,"wb")
                     json.dump(obj2dict(self.sheet),fid)
                     fid.close()
@@ -47,9 +68,31 @@ class Interface:
                 json.dump(obj2dict(self.sheet),fid)
                 fid.close()
                 loop_cond = False
+'''    def updateSheet(self,filename,param_list):
+        loop_cond = True
+        while(loop_cond):
+            if os.path.isfile(filename):
+                o_write = raw_input("Character sheet already exists. Overwrite?[Y/n]")
+                print o_write
+                if o_write == 'Y':
 
+                    fid = open(filename,"wb")
+                    json.dump(obj2dict(self.sheet),fid)
+                    fid.close()
+                    loop_cond = False
+                elif o_write == 'n':
+                    print "Overwrite aborted."
+                    loop_cond = False
+                else:
+                    print "Not a valid option."
+            else:
+                fid = open(filename,"wb")
+                json.dump(obj2dict(self.sheet),fid)
+                fid.close()
+                loop_cond = False
+'''
 myInterface = Interface()
-#myInterface.createSheet("test1.json")
+myInterface.createSheet("test1.json")
 myInterface.retrieveSheet("test1.json")
 pp = pprint.PrettyPrinter(indent=1,width=1)
 pp.pprint(myInterface.sheet.total_stats)
--- charSheet.py
+++ charSheet.py
@@ -2,11 +2,30 @@ class Sheet:
 
     def __init__(self,**kwargs):
         if kwargs:
+            self.name = kwargs["name"]
+            self.race = kwargs["race"]
+            self.char_class = kwargs["char_class"]
+            self.alignment = kwargs["alignment"]
+            self.background = kwargs["background"]
+            self.lvl = kwargs["lvl"]
+            self.exp = kwargs["exp"]
+
             self.base_stats = kwargs["base_stats"]
             self.bonus_stats = kwargs["bonus_stats"]
             self.total_stats = kwargs["total_stats"]
+
+            self.p_bonus = kwargs["p_bonus"]
+            self.s_throws = kwargs["s_throws"]
+            self.skills = kwargs["skills"]
+
+
+            self.ac = kwargs["ac"]
+            self.initiative = kwargs["initiative"]
+            self.speed = kwargs["speed"]
+            self.hp = kwargs["hp"]
+            self.tmp_hp = kwargs["tmp_hp"]
+            self.hit_die = kwargs["hit_die"]
         else:
-            self.name = "No Name"
             self.base_stats = {
             "str":0,
             "dex":0,
@@ -31,6 +50,25 @@ class Sheet:
             "wis":0,
             "cha":0
             }
+            self.name = "no name"
+            self.race = "no race"
+            self.char_class = "no class"
+            self.alignment = "no alignment"
+            self.background = "no background"
+            self.lvl = 1
+            self.exp = 0
+
+            self.p_bonus = 0
+            self.s_throws = "none"
+            self.skills = "none"
+
+            self.ac = 0
+            self.initiative = 0
+            self.speed = 0
+            self.hp = 0
+            self.tmp_hp = 0
+            self.hit_die = "1d6"
+
     def updateBonusStat(self,stat,val):
         self.bonus_stats[stat] = val
         print self.bonus_stats[stat]
